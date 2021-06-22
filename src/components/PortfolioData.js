37baaeb69c3d838d79d609c3bc222b1af3e5fb45
Tamas Zentai <zentaitamas@gmail.com>
2021-06-22 20:05:01 +0100

    add more projects, change font-style

--- src/components/PortfolioData.js
+++ src/components/PortfolioData.js
@@ -3,6 +3,8 @@ import GleArt from "../assets/gleart.png";
 import SpaceX from "../assets/spacex.png";
 import BrainTeaser from "../assets/brainteaser.png";
 import Portfolio from "../assets/portfolio.png";
+import EHR from "../assets/ehr.png";
+
 
 export const PortfolioData = [
   {
@@ -19,7 +21,7 @@ export const PortfolioData = [
     url: "https://gleart.com",
     alt: "GleArt preview",
     description:
-      "Portfolio app that allows the owner to upload their work (arts) to show off.  The project uses Nuxt.js for Search Engine Optimization, Strapi headless CMS for managing content, various CSS frameworks with custom CSS, EmailJS for sending messages. The client-side hosted on Google Firebase using Firebase CLI, Strapi CMS is hosted on Google Cloud Platform. Working on this project helped solidify my understanding of Vue.js used by Nuxt and gain experience working with CMS and cloud technologies.",
+      "Portfolio app that allows the owner to upload their work (arts) to show off. The project uses Nuxt.js for Search Engine Optimization, Strapi headless CMS for managing content, various CSS frameworks with custom CSS, EmailJS for sending messages. The client-side hosted on Google Firebase using Firebase CLI, Strapi CMS is hosted on Google Cloud Platform. Working on this project helped solidify my understanding of Vue.js used by Nuxt and gain experience working with CMS and cloud technologies.",
   },
   {
     title: "SpaceX Launches Tracker",
@@ -28,6 +30,13 @@ export const PortfolioData = [
     alt: "SpaceX preview",
     description:
       "A single-page application where the user can check every SpaceX rocket launch in detail can sort the launches in ascending / descending order and filter the data by year. Pagination and modal view are also included. The data provided by a public API. Working on this project helped to solidify my understanding of Component-based approach) and gain experience working with REST APIs.",
+  },
+  {
+    title: "Electronic Health Records",
+    img: EHR,
+    url: "https://github.com/tamaszentai/EHR",
+    alt: "EHR preview",
+    description: "This was a given coding challenge from a company to make a REST API only but I felt this is a good project so I developed a simple React based front-end to it. For this project I used Node.js, Express.js, PostgreSQL and React."
   },
   {
     title: "BrainTeaser",
